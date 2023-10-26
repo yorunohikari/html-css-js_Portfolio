30118c6ee44f81fba4d6e376033636b87b4ae27d
yorunohikari <90948140+yorunohikari@users.noreply.github.com>
2023-10-26 08:41:08 +0700

    adding home button function

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 } 
+
+function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+  
+  // Add an event listener to show/hide the arrow based on the scroll position
+  window.addEventListener('scroll', function() {
+    var scrollToTopArrow = document.getElementById('scrollToTopArrow');
+    if (window.scrollY > 700) { // Adjust the scroll position at which the arrow should appear
+      scrollToTopArrow.classList.add('show');
+    } else {
+      scrollToTopArrow.classList.remove('show');
+    }
+  });
